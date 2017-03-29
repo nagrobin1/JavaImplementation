fc4ce17b53ede5c92c02d7d3885a03fb3a4b74e5
robinnag <nagrobin1@gmail.com>
2017-03-29 14:46:40 -0400

    Binary Search Tree

--- src/BST/BinarySearchTree.java
+++ src/BST/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package BST;
 
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by test on 3/25/17.
@@ -18,7 +20,7 @@ class Node {
     }
 }
 
-public class BinarySearchTree {
+public class BinarySearchTree{
 
     // Root of BST
     Node root;
@@ -72,73 +74,178 @@ public class BinarySearchTree {
         System.out.println(ptr.key);
     }
 
-    void preOrderTransversal(){
+    public ArrayList preOrderTransversal(){
+        ArrayList answer = new ArrayList();
         // If empty tree, return nothing
         if(this.root == null){
-            return;}
+            return answer;}
+        preOrder(this.root, answer);
+        return answer;}
 
-        preOrder(this.root);
-        System.out.println();}
-
-    private void preOrder(Node ptr){
+    private void preOrder(Node ptr, ArrayList answer){
         // Base Case
         if(ptr == null){
             return;}
 
         // root, leftsubtree, rightsubtree
-        System.out.print(ptr.key + ", ");
-        preOrder(ptr.left);
-        preOrder(ptr.right);
+        answer.add(ptr.key);
+        // Process the left and right subtree
+        preOrder(ptr.left, answer);
+        preOrder(ptr.right, answer);
     }
 
-    void inOrderTransversal(){
+    public ArrayList inOrderTransversal(){
+        ArrayList answer = new ArrayList();
         // If empty tree, return nothing
         if(this.root == null){
-            return;}
+            return answer;}
 
-        inOrder(this.root);
-        System.out.println();}
+        inOrder(this.root, answer);
+        return answer;}
 
-    private void inOrder(Node ptr){
+    private void inOrder(Node ptr, ArrayList answer){
         // Base Case
         if(ptr == null){
             return;}
 
         // leftsubtree, root, rightsubtree
-        inOrder(ptr.left);
-        System.out.print(ptr.key + ", ");
-        inOrder(ptr.right);
+        inOrder(ptr.left, answer);
+        answer.add(ptr.key);
+        inOrder(ptr.right, answer);
     }
 
-    void postOrderTraversal(){
+    public ArrayList postOrderTraversal(){
 
+        ArrayList answer = new ArrayList();
         // If empty tree, return nothing
         if(this.root == null){
-            return;}
+            return answer;}
 
-        postOrder(this.root);
-        System.out.println();
+        postOrder(this.root, answer);
+        return answer;
     }
 
-    private void postOrder(Node ptr){
+    private void postOrder(Node ptr, ArrayList answer){
         // Base Case
         if(ptr == null){
             return;}
 
         // Left-subtree, Right-subtree, Root
-        postOrder(ptr.left);
-        postOrder(ptr.right);
-        System.out.print(ptr.key + ", ");
+        postOrder(ptr.left, answer);
+        postOrder(ptr.right, answer);
+        answer.add(ptr.key);
     }
 
-    void BreadthFirstSearch(){
+    public ArrayList BreadthFirstSearch(){
+        ArrayList answer = new ArrayList();
         // If empty tree, return nothing
         if(this.root == null){
-            return;}
+            return answer;}
 
-        PriorityQueue<Interger> = new
+        Node ptr = this.root;
+        Queue<Node> P = new LinkedList<>();
 
+        P.add(ptr);
 
+        while(P.isEmpty() == false){
+            if(P.peek() == null){
+                P.remove();
+                continue;
+            }
+            answer.add(P.peek().key);
+            Node parent = P.remove();
+            if(parent == null){
+                P.remove();
+                continue;}
+                P.add(parent.left);
+                P.add(parent.right);
+        }
+        return answer;
+    }
+
+    public int sumOfCoveredNodes(){
+        ArrayList uncovered = new ArrayList();
+        // If empty tree, return nothing
+        if(this.root == null){
+            return 0;}
+
+        Node ptr = this.root;
+        uncovered.add(this.root.key);
+        // Get the items of the right boundry
+        // go to the most right-most node
+        while(ptr.right != null){
+            uncovered.add(ptr.right.key);
+            ptr = ptr.right;
+        }
+
+        ptr = this.root;
+        // Get the items of the left boundry
+        // go to the most right-most node
+        while(ptr.left != null){
+            uncovered.add(ptr.left.key);
+            ptr = ptr.left;
+        }
+
+        System.out.println("Uncovered nodes are" + uncovered);
+
+        // get the all the items the whole tree
+        ArrayList allNodes = this.BreadthFirstSearch();
+
+        // Calculate sum of all nodes
+        int sumAll = 0;
+        for(int i = 0; i < allNodes.size(); i++){
+            sumAll += (int)allNodes.get(i);
+        }
+
+        // Calculate sum of all uncoveered nodes
+        int sumUncovered = 0;
+        for(int i = 0; i < uncovered.size(); i++){
+            sumUncovered += (int)uncovered.get(i);
+        }
+
+        int difference = sumAll - sumUncovered;
+
+        return difference;
+    }
+
+    public int shortestPath(){
+        ArrayList<Integer> pathDistances = new ArrayList<Integer>();
+        // If empty tree, return nothing
+        if(this.root == null){
+            return 0;}
+
+        Node ptr = this.root;
+
+        calculateShortestPath(0, ptr, pathDistances);
+        // calculateShortestPath(this.root.key, ptr.left, pathDistances);
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < pathDistances.size(); i++){
+            if(pathDistances.get(i) < min){
+                min = pathDistances.get(i);
+            }
+        }
+        return min;
+    }
+
+    private void calculateShortestPath(int sumsoFar, Node ptr, ArrayList<Integer> pathDistances) {
+        // if-elseif-else statements stop doing comparisons as soon as it finds one that's true.
+        // if-if-if does every comparison
+        // reached a leag node, so put the path cost inside the array list
+        if (ptr.right == null && ptr.left == null) {
+            pathDistances.add(sumsoFar + ptr.key);
+            return;
+        } // if only has left child
+        else if (ptr.right == null){
+            calculateShortestPath(sumsoFar + ptr.key, ptr.left, pathDistances);
+        } // if only has right child
+        else if (ptr.left == null) {
+            calculateShortestPath(sumsoFar + ptr.key, ptr.right, pathDistances);
+        } // if has both children
+        else {
+            calculateShortestPath(sumsoFar + ptr.key, ptr.right, pathDistances);
+            calculateShortestPath(sumsoFar + ptr.key, ptr.left, pathDistances);
+        }
     }
 
 }
--- src/BST/BreadthFirstSearch.java
+++ src/BST/BreadthFirstSearch.java
@@ -1,7 +1,6 @@
 package BST;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * Created by test on 3/25/17.
@@ -12,36 +11,44 @@ public class BreadthFirstSearch extends BinarySearchTree {
 
     public static void main(String [] args){
 
+            /* Let us create following BST
+              15
+           /     \
+          11      20
+         /  \    /  \
+        8   12  17  25
+       /             \
+      7             30 */
+
+
         BinarySearchTree B = new BinarySearchTree();
 
         B.insert(15);
-        B.insert(10);
+        B.insert(11);
         B.insert(20);
         B.insert(8);
         B.insert(12);
         B.insert(17);
         B.insert(25);
         B.insert(30);
+        B.insert(7);
 
-        B.findMax();
+        System.out.println("The largest item is "); B.findMax();
         B.findMin();
         // Depth First Search
-        B.preOrderTransversal();
-        B.inOrderTransversal();
-        B.postOrderTraversal();
+        ArrayList preorder = B.preOrderTransversal();
+        System.out.println(preorder);
+        ArrayList inorder = B.inOrderTransversal();
+        System.out.println(inorder);
+        ArrayList postorder = B.postOrderTraversal();
+        System.out.println(postorder);
+        ArrayList bfs = B.BreadthFirstSearch();
+        System.out.println(bfs);
 
-        HashMap<Integer, Integer> r = new HashMap<Integer, Integer>();
-
-        Map<Integer, Integer> s = new HashMap<>();
-
-        Has
+        System.out.println(B.sumOfCoveredNodes());
+        // shortest path from root to leaf 15-11-12
+        System.out.println(B.shortestPath());
     }
-
-
-
-
-
-
 
 
 }
